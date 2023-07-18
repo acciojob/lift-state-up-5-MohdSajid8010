25db897f1b333f8d69bdf387268003e2c5e5ac51
sajid <mohdsajid8010@gmail.com>
2023-07-18 22:30:01 +0530

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [formData, setFormData] = useState({ name: "", password: "" })
+  const [isLoggedIn, setisLoggedIn] = useState(false)
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div >
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      {
+        isLoggedIn ?
+          (
+            <p>You are logged in!</p>
+          ) :
+          (
+            <div>
+              <label htmlFor="Name">Username:</label>
+              <input type="text" id="Name" onChange={(e) => setItem({ ...formData, name: e.target.value })} />
+
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" onChange={(e) => setItem({ ...formData, password: e.target.value })} />
+
+              <button onClick={() => setisLoggedIn(true)}>Login</button>
+            </div>
+          )
+      }
+
+
+
+
+
     </div>
   )
 }
